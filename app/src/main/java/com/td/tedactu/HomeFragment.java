9b786314f77df4ab57b2df827cfaaba13ab22354
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2019-06-27 23:09:08 -0400

    bottom part 1
    implementation done

---
+++ app/src/main/java/com/td/tedactu/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.td.tedactu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_home, parent, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+
+}
