ef744f40c1f442964485df24d0f05765ea5650c8
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2020-05-02 10:24:16 -0400

    landscape part 1

--- app/src/main/java/com/td/tedactu/CultureFragment.java
+++ app/src/main/java/com/td/tedactu/CultureFragment.java
@@ -1,4 +1,24 @@
 package com.td.tedactu;
 
-public class CultureFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CultureFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_culture, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+
 }
--- app/src/main/java/com/td/tedactu/SportFragment.java
+++ app/src/main/java/com/td/tedactu/SportFragment.java
@@ -1,4 +1,24 @@
 package com.td.tedactu;
 
-public class SportFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SportFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_sport, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+
 }
